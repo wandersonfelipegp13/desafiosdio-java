a7a63c87ec57a67bbd9c7518fa5fd81909ba7ab6
Wanderson Felipe <wandersonfelipeplus13@gmail.com>
2023-01-29 21:06:36 -0300

    feat: Taxa de Crescimento

---
+++ src/desafios/TaxaDeCrescimento.java
@@ -0,0 +1,32 @@
+package desafios;
+
+import java.util.Scanner;
+
+/**
+ * Esse programa calcula a quantidade de
+ * anos que a população do país A leva
+ * para atingir, ou superar, a população
+ * do país B, baseados em sua taxa de
+ * crescimento.
+ */
+public class TaxaDeCrescimento {
+
+    public static void main(String[] args) {
+        double a, b;
+        Scanner input = new Scanner(System.in);
+        double taxa = 1.5;
+
+        a = input.nextInt();
+        b = input.nextInt();
+
+        int anos = 1;
+
+        while (a < b) {
+            a += (a * taxa / 100);
+            anos++;
+        }
+
+        System.out.println(anos + " anos");
+    }
+
+}
