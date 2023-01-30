7b5f90427dba07893c6ca8baddf41f3fa7eedb0e
Wanderson Felipe <wandersonfelipeplus13@gmail.com>
2023-01-29 21:50:10 -0300

    feat: Tempo de Download

---
+++ src/desafios/TempoDeDownload.java
@@ -0,0 +1,23 @@
+package desafios;
+
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+/**
+ * Esse programa pede o tamanho, em MB, de
+ * um arquivo para download e a velocidade,
+ * em Mbps, de um link de Internet e calcula
+ * o tempo aproximado de download do arquivo,
+ * em minutos, usando este link.
+ */
+public class TempoDeDownload {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double tmhMB = sc.nextFloat();
+        double tmhMbps = sc.nextFloat();
+
+        DecimalFormat df = new DecimalFormat("0.00");
+        System.out.println( df.format( ((tmhMB * 8) / tmhMbps) / 600 ));
+    }
+}
