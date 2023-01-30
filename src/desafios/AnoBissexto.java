5c89e0deb9f7fc160184486ce8879d29e43afa85
Wanderson Felipe <wandersonfelipeplus13@gmail.com>
2023-01-29 22:29:19 -0300

    feat: Ano Bissexto

---
+++ src/desafios/AnoBissexto.java
@@ -0,0 +1,27 @@
+package desafios;
+
+import java.util.Scanner;
+
+public class AnoBissexto {
+    public static void main(String[] args) {
+        Scanner ent = new Scanner(System.in);
+        int ano;
+
+        ano = ent.nextInt();
+
+        // Anos ímpares e anos pares, que não são
+        // multíplos de 4, não são bissextos. Então,
+        // não precisamos validar se o ano é impar,
+        // pois, qualquer número impar, dividido por
+        // um número par, resulta em algum resto, e
+        // isso é feito quando verificamos se o ano
+        // é múltiplo de 4. Sendo assim, só precisamos
+        // validar se o ano é multiplo de 4.
+        if (ano % 4 != 0) {
+            System.out.println("Nao Bissexto");
+        }
+        else {
+            System.out.println("Bissexto");
+        }
+    }
+}
